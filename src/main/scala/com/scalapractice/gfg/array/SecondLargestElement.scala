d21c8e8d737e5d0750df5fcd3beb1881fe6ef9bf
kartik chandra kudada <kartik.kudada@exate.com>
2023-05-05 17:10:56 +0530

    second largest number

--- src/main/scala/com/scalapractice/gfg/array/SecondLargestElement.scala
+++ src/main/scala/com/scalapractice/gfg/array/SecondLargestElement.scala
@@ -8,8 +8,14 @@ object SecondLargestElementInArray {
 
   def main(args: Array[String]): Unit = {
 
-    val array = Array(12, 35, 1, 10, 34, 1)
+    val array = Array(12, 35, 1,87,10, 34, 1,12,100,97)
     println(s" second largset element : ${method1(array)}")
+
+    println(s" second largset element (method2) : ${method2(array)}")
+
+    println(s" second largset element (method3) : ${method3(array)}")
+
+    println(s" second largset element (method4) : ${method4(array)}")
   }
 
   def method1(input:Array[Int]) :Int = {
@@ -18,5 +24,50 @@ object SecondLargestElementInArray {
     val len = input.length
     println(s"$sortedList")
     sortedList(len-2)
+  }
+
+  def method2(input:Array[Int]) : Int = {
+   var list = input.toList
+   val max = input.max
+  val list2 =   list.filter( _ < max)
+    list2.max
+  }
+
+  def method3(input:Array[Int]) : Int = {
+    var first = Integer.MIN_VALUE
+    var second = Integer.MIN_VALUE
+    input.foreach(x => {
+       first = Math.max(x,first)
+    })
+
+    input.foreach(x => {
+      if(x != first )
+       {
+          second = Math.max(second,x)
+       }
+    })
+    second
+  }
+
+  /*
+   with only one iteration
+   */
+  def method4(input:Array[Int]) : Int = {
+    var first = Int.MinValue
+    var second = Int.MinValue
+    // Array(12, 35, 1,87,10, 34, 1,12,100,97)
+    // first   12  35 87 100
+    // second  min 12 35 87 97
+    input.foreach( x => {
+      if(x > first)
+        {
+          second = first
+          first = x
+        }
+
+      if(first > x && x > second )
+         second = x
+    })
+  second
   }
 }
