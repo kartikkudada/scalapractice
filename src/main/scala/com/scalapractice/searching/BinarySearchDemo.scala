cabb2958dc10c82067aaebf2230701a45c827861
kartik chandra kudada <kartik.kudada@exate.com>
2023-05-13 11:57:28 +0530

    binarysearch

--- src/main/scala/com/scalapractice/searching/BinarySearchDemo.scala
+++ src/main/scala/com/scalapractice/searching/BinarySearchDemo.scala
@@ -6,20 +6,38 @@ object BinarySearchDemo {
 
   def main(args: Array[String]): Unit = {
     val array = Array(2, 5, 8, 12, 16, 23, 38, 56, 72, 91)
-    println(s"  ${binarySearch(array,911,0,array.length-1)}")
+    val input = Array(100,10,50,21,30,15)
+   /* println(s"  ${binarySearch(array,56,0,array.length-1)}")
+    println(s" recurssive  ${binarySearchTail(array,72,0,array.length-1)}")*/
+    println( s"  ---  ${java.util.Arrays.binarySearch(input,21)}  ")
+    println( s"  ---  ${java.util.Arrays.binarySearch(array,8)}  ")
   }
+
   def binarySearch(input:Array[Int], key:Int, low:Int, high:Int) : Int = {
-    if(high >= low) {
+    if( low > high )
+      -1
       val mid = low + (high - low) / 2
+
       if (input(mid) == key) {
-        return  mid
+       return mid
       }
       else if (input(mid) > key)
-        return  binarySearch(input,key,low,mid-1)
-      else (input(mid) < key)
-        return  binarySearch(input,key,mid+1,high)
-    }
-    -1
+        return binarySearch(input, key,low, mid - 1)
+        else
+        return binarySearch(input, key,mid + 1, high)
+  }
+
+  @tailrec
+  def binarySearchTail(input:Array[Int], key:Int,low:Int, high:Int): Int =
+  {
+    if(low > high) return -1
+    val mid = low + (high-low)/2
+
+     input(mid) match {
+       case i if(i == key) => mid
+       case j if(j < key) => binarySearchTail(input,key,mid+1,high)
+       case _ => binarySearchTail(input,key,low,mid-1)
+        }
   }
 
 }
--- src/main/scala/com/scalapractice/searching/LinearSearchDemo.scala
+++ src/main/scala/com/scalapractice/searching/LinearSearchDemo.scala
@@ -10,18 +10,15 @@ object LinearSearchDemo {
   val array = Array(10, 50, 30, 70, 80, 20, 90, 4)
 
   def main(args: Array[String]): Unit = {
-    println( s" searching 20 in array :  ${linearSearch1(array,20)}")
 
-    println( s" searching 4 in array, Found in position:  ${linearSearch2(array,4)}")
+    println( s" searching 4 in array, Found in position:  ${linearSearch(array,4)}")
 
-    println( s" searching 70 in array recursive way, Found in position:  ${linearSearchrecussive(array,0,4)}")
+    println( s" searching 200 in array recursive way, Found in position:  ${linearSearchTailrecussive(array,0,200)}")
+
+    println( s" searching 70 in array recursive way, Found in position:  ${linearSearchTailrecussive(array,0,70)}")
+
   }
-
-  def linearSearch1(input:Array[Int],data:Int):Boolean = {
-    !input.filter(_ == data ).isEmpty
-  }
-
-  def linearSearch2(input:Array[Int],data:Int):Int = {
+  def linearSearch(input:Array[Int],data:Int):Int = {
     for(index <- 0 until input.length)
       {
         input(index) == data match {
@@ -33,7 +30,7 @@ object LinearSearchDemo {
   }
 
   @tailrec
-  def linearSearchrecussive(input:Array[Int],index:Int,data:Int) :Int = {
+  def linearSearchTailrecussive(input:Array[Int],index:Int,data:Int) :Int = {
 
     if(index == input.length-1 && input(index) != data)
       -1
@@ -41,7 +38,19 @@ object LinearSearchDemo {
       index
     }
     else {
-        linearSearchrecussive(input,index+1,data)
+      linearSearchTailrecussive(input,index+1,data)
      }
   }
+
+
+  def linearSearchRecussive(input: Array[Int], index: Int, data: Int): Int = {
+    if (input(index) == data)
+       index
+    else if(index < input.length)
+      linearSearchRecussive(input, index + 1, data)
+   else (index == input.length - 1 && input(index) != data)
+      -1
+  }
+
+
 }
